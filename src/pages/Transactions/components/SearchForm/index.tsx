ec3371d4114d72727cb7f173d2b8a83bee2f000e
Rafael Perini <rafa1999perini@gmail.com>
2024-02-01 15:54:43 -0300

    first commit

---
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -0,0 +1,69 @@
+import { MagnifyingGlass } from "phosphor-react";
+import { SearchFormContainer } from "./styles";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { TransactionsContext } from "../../../../contexts/TransactionsContext";
+import { useContextSelector } from "use-context-selector";
+import { memo } from "react";
+
+/**
+ * Por que que um componente renderiza?
+ * 
+ * - Hooks changed (mudou estado, contexto, reducer)
+ * - Props changed (mudou propriedades)
+ * - Parent render (renderizou o componente pai)
+ * 
+ * Qual o fluxo de renderização de um componente?
+ * 
+ * 1. O React recria o HTML da interface daquele componente
+ * 2. O React compara o HTML antigo com o HTML novo
+ * 3. Se mudar alguma coisa, o React reescreve o HTML na tela
+ * 
+ * Memo: 
+ * 0. Hooks changed, Props changed (deep comparison)
+ * 0.1. Comparar a versão anterior dos hooks e props com a versão atual
+ * 0.2. SE mudou algo, ele vai permitir a nova renderização
+ * 
+ */
+
+const searchFormSchema = z.object({
+   query: z.string(),
+})
+
+type SearchFormInputs = z.infer<typeof searchFormSchema>;
+
+function SearchFormComponent() {
+   const fetchTransactions = useContextSelector(TransactionsContext, (context) => {
+      return context.fetchTransactions;
+   })
+
+   const { 
+      register, 
+      handleSubmit,
+      formState: { isSubmitting }
+   } = useForm<SearchFormInputs>({
+      resolver: zodResolver(searchFormSchema)
+   })
+
+   async function handleSearchTransactions(data: SearchFormInputs) {
+      await fetchTransactions(data.query)
+   }
+
+   return (
+      <SearchFormContainer onSubmit={handleSubmit(handleSearchTransactions)}>
+         <input 
+            type="text" 
+            placeholder="Busque por transações" 
+            {...register("query")}
+         />
+
+         <button type="submit" disabled={isSubmitting}>
+            <MagnifyingGlass size={20} />
+            Buscar
+         </button>
+      </SearchFormContainer>
+   )
+}
+
+export const SearchForm = memo(SearchFormComponent);
